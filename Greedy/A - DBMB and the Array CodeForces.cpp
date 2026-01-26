43a8a42cac15dd7b115ab0be26ada9e54c3113e2
Abo Elfadl Ramadan <150173477+abwalfdlrmdan128@users.noreply.github.com>
2026-01-26 21:52:33 +0200

    Create A - DBMB and the Array CodeForces.cpp

---
+++ Greedy/A - DBMB and the Array CodeForces.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=3e5+5;
+const ll LIM=1e18;
+ll Gcd(ll a,ll b){ return b?gcd(b,a%b):a;}
+bool compare(ll &x,ll &y) {
+    return x>y;
+}
+bool isSuffix(const string& a, const string& b) {
+    if (a.size() > b.size()) return false;
+    return b.substr(b.size() - a.size()) == a;
+}
+vector<vector<ll>>Adj(N);
+vector<ll>sz(N);
+bool visitde[N];
+void DFS(ll u) {
+    visitde[u]=true;
+    sz[u]=1;
+    for (ll v : Adj[u]) {
+        if (!visitde[v]) {
+            DFS(v);
+            sz[u]+=sz[v];
+        }
+    }
+}
+vector<vector<int>>dp;
+string x,y;
+int n;
+int solve(int i,bool f) {
+    if(i==n)return 0;
+    if(dp[i][f]!=-1)return dp[i][f];
+    int res=INT_MAX;
+    int xi=f^0;
+    int cost=(x[i]-'0'!=xi)+(y[i]-'0'!=0);
+    res=min(res,cost+solve(i+1,0));
+
+    xi=f^1;
+    cost=(x[i]-'0'!=xi)+(y[i]-'0'!=1);
+    res=min(res,cost+solve(i+1,1));
+    return dp[i][f]=res;
+}
+void query() {
+  int n,s,x;
+    cin>>n>>s>>x;
+    vector<int>vec(n);
+    ll sum=0;
+    for(int i=0;i<n;i++) {
+        cin>>vec[i];
+        sum+=vec[i];
+    }
+    if(sum==s)cout<<"YES"<<endl;
+    else {
+        if(sum<=s&&(sum-s)%x==0)cout<<"YES"<<endl;
+        else {
+            cout<<"NO"<<endl;
+        }
+    }
+
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--) {
+        query();
+    }
+    return 0;
+}
